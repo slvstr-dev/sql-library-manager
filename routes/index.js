04e1e306f420f2052a8019f57dc5e8e5d38ff4af
Sylvester Hofstra <hello@slvstr.dev>
2021-05-13 12:42:46 +0200

    Replace var with let & const.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var router = express.Router();
-var Book = require("../models").Book;
+const express = require("express");
+const router = express.Router();
+const Book = require("../models").Book;
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
