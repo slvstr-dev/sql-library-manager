1906b36e51ed907735d1b7293e0020c9a0d5daa1
Sylvester Hofstra <hello@slvstr.dev>
2021-05-13 13:14:01 +0200

    Create initial pug views based on example mark-up.

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
+// test connection to the database
 (async () => {
     try {
         await sequelize.authenticate();
