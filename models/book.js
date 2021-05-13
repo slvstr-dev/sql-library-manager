f49eb7eadb3a42e2854c2efb6ea60964ed9ac82c
Sylvester Hofstra <hello@slvstr.dev>
2021-05-13 12:18:16 +0200

    Add Book model restrictions & validations.

--- models/book.js
+++ models/book.js
@@ -1,26 +1,47 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Book extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Book extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  };
-  Book.init({
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    year: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Book',
-  });
-  return Book;
+    Book.init(
+        {
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: 'Please provide a value for "title".',
+                    },
+                },
+            },
+            author: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: 'Please provide a value for "author".',
+                    },
+                },
+            },
+            genre: {
+                type: DataTypes.STRING,
+            },
+            year: {
+                type: DataTypes.INTEGER,
+            },
+        },
+        {
+            sequelize,
+            modelName: "Book",
+        }
+    );
+    return Book;
 };
